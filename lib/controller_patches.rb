456f96ff6829c6a33ba864e01a4b4c626951a9cb
lizconlan <liz@mysociety.org>
2017-07-07 17:26:36 +0100

    Enable editing of postcode and province

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -28,6 +28,70 @@ Rails.configuration.to_prepare do
   end
 
   UserController.class_eval do
+    def signchangeprovince
+      if !authenticated?(
+        :web => _('To change your province used on {{site_name}}',
+                  :site_name => site_name),
+        :email => _('Then you can change your province used on {{site_name}}',
+                    :site_name => site_name),
+        :email_subject => _('Change your province used on {{site_name}}',
+                            :site_name => site_name)
+        )
+        # "authenticated?" has done the redirect to signin page for us
+        return
+      end
+
+      if !params[:submitted_signchangeprovince_do]
+        render :action => 'signchangeprovince'
+        return
+      else
+        @user.province = params[:signchangeprovince][:province]
+        if not @user.valid?
+          @signchangeprovince = @user
+          render :action => 'signchangeprovince'
+        else
+          @user.save!
+          # Now clear the circumstance
+          flash[:notice] = _('You have now changed your province used on ' \
+                             '{{site_name}}',
+                             :site_name => site_name)
+          redirect_to user_url(@user)
+        end
+      end
+    end
+
+    def signchangepostcode
+      if !authenticated?(
+        :web => _('To change your postcode used on {{site_name}}',
+                  :site_name => site_name),
+        :email => _('Then you can change your postcode used on {{site_name}}',
+                    :site_name => site_name),
+        :email_subject => _('Change your postcode used on {{site_name}}',
+                            :site_name => site_name)
+        )
+        # "authenticated?" has done the redirect to signin page for us
+        return
+      end
+
+      if !params[:submitted_signchangepostcode_do]
+        render :action => 'signchangepostcode'
+        return
+      else
+        @user.postcode = params[:signchangepostcode][:postcode]
+        if not @user.valid?
+          @signchangeprovince = @user
+          render :action => 'signchangepostcode'
+        else
+          @user.save!
+          # Now clear the circumstance
+          flash[:notice] = _('You have now changed your postcode used on ' \
+                             '{{site_name}}',
+                             :site_name => site_name)
+          redirect_to user_url(@user)
+        end
+      end
+    end
+
     # Add our extra params to the sanitized list allowed at signup
     def user_params(key = :user)
       params.require(key).permit(:name, :email, :password,
