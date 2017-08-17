2d2d02e1ad7ed8e6afff2f269bf22f229a80d41f
lizconlan <liz@mysociety.org>
2017-08-17 15:37:46 +0100

    Refactor User.province_name_options
    * Move the list of province names to a private Hash containing
      one array per locale name (or default as the catch-all option)
    * Simplify User.province_name_options to pick the specified locale
      name by key or fall through to the default array
    
    (Simplifies validation by having all the names stored in one structure)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,29 @@ describe User do
     expect{ user.province }.to_not raise_error
   end
 
+  describe '.province_name_options' do
+
+    it 'returns the French names by default' do
+      expect(User.province_name_options).to include('Bruxelles')
+    end
+
+    it 'returns a specific locale if found' do
+      # TODO - tidy this up when we get the locale handling library (0.30?)
+      begin
+        available_locales = I18n.available_locales
+        I18n.available_locales << :'nl-BE'
+        FastGettext.default_available_locales =
+          I18n.available_locales.map { |x| x.to_s.gsub('-', '_') }
+        I18n.with_locale('nl_BE') do
+          expect(User.province_name_options).to include('Brussel')
+        end
+      ensure
+        I18n.available_locales = available_locales
+        FastGettext.available_locales =
+          I18n.available_locales.map { |x| x.to_s.gsub('-', '_') }
+      end
+    end
+
+  end
+
 end
