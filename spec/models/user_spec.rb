f44526d09f0d071659b91e69833052eb89c8de35
lizconlan <liz@mysociety.org>
2017-08-17 13:42:37 +0100

    Add integration specs for postcode and province
    Covers:
      * Signing up with a postcode
      * Signing up without a postcode
      * Changing postcode
      * Removing postcode (setting to blank)
      * Signing up with a province
      * Signing up without a province
      * Changing province

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,5 @@
 # -*- encoding : utf-8 -*-
-ALAVETELI_TEST_THEME = 'belgium-theme'
-require File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','..','..','spec','spec_helper'))
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe User do
 
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+# -*- encoding : utf-8 -*-
+ALAVETELI_TEST_THEME = 'belgium-theme'
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','..','spec','spec_helper'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','..','spec','integration','alaveteli_dsl'))
